ea2cdc5e808f33a9735069d857ba80a30467b192
Marcos Vinicius <marcos.carini22@gmail.com>
2023-11-28 23:29:02 -0300

    feat: add ListPage class

---
+++ src/test/java/org/example/objects/ListPage.java
@@ -0,0 +1,20 @@
+package org.example.objects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class ListPage {
+    private final WebDriver driver;
+    public ListPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void open() {
+        driver.get("http://127.0.0.1:5500/index.html");
+    }
+
+    public boolean verifyIfListIsEmpty() {
+        String path = String.format("//tbody/tr[%d]/td", 1);
+        return driver.findElements(By.xpath(path)).isEmpty();
+    }
+}
