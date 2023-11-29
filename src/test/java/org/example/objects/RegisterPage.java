f2001a9e99982ee95d484201606a04954bebff14
Eduardo <eduardocorreass25@gmail.com>
2023-11-28 21:07:53 -0300

    feat: add RegisterPage class to objects

---
+++ src/test/java/org/example/objects/RegisterPage.java
@@ -0,0 +1,55 @@
+package org.example.objects;
+
+import com.github.javafaker.Faker;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class RegisterPage {
+    private final WebDriver driver;
+    private final Faker faker = new Faker();
+
+    public RegisterPage(WebDriver driver) {
+       this.driver = driver;
+    }
+
+    public void open() {
+        driver.get("http://127.0.0.1:5500/forge.html");
+    }
+
+    public void setName(String name){
+        WebElement soulName = driver.findElement(By.id("name"));
+        soulName.clear();
+        soulName.sendKeys(name);
+    }
+
+    public void setDateOfDeath(String dateOfDeath){
+        WebElement soulDateOfDeath = driver.findElement(By.id("dateOfDeath"));
+        soulDateOfDeath.clear();
+        soulDateOfDeath.sendKeys(dateOfDeath);
+    }
+
+    public void setDateBirthday(String birthday){
+        WebElement soulBirthday = driver.findElement(By.id("birthday"));
+        soulBirthday.clear();
+        soulBirthday.sendKeys(birthday);
+    }
+
+    public void setCauseOfDeath(String causeOfDeath){
+        WebElement soulCauseOfDeath = driver.findElement(By.id("causeOfDeath"));
+        soulCauseOfDeath.clear();
+        soulCauseOfDeath.sendKeys(causeOfDeath);
+    }
+
+    public void clickOnCreateButton() {
+        driver.findElement(By.cssSelector(".btn.btn-primary")).click();
+    }
+
+    public void forgeSoul() {
+        setName(faker.name().fullName());
+        setDateOfDeath(faker.date().toString());
+        setDateBirthday(faker.date().toString());
+        setCauseOfDeath(faker.bothify("Ataque Cardíaco"));
+        clickOnCreateButton();
+    }
+}
